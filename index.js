75ec45335818c3c2c5772c9338e3fa2616721523
Paulo Afonso <pauloferrisouza@gmail.com>
2022-05-17 16:06:38 -0300

    create route to get talker by id

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ app.get('/talker', (_req, res) => {
   return res.status(HTTP_OK_STATUS).json(talker);
 });
 
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const talker = JSON.parse(fs.readFileSync('talker.json'));
+  console.log(talker);
+  const filteredTalker = talker.find((t) => t.id === +id);
+  if (filteredTalker) return res.status(HTTP_OK_STATUS).json(filteredTalker);
+  if (!filteredTalker) {
+ return res.status(404).json(
+    { message: 'Pessoa palestrante não encontrada' },
+    ); 
+}
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
