0a087c37e218bafd95b88faf7545c38124abd733
Paulo Afonso <pauloferrisouza@gmail.com>
2022-05-17 14:57:45 -0300

    create route to get all the registered talkers

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_req, res) => {
+  const talker = JSON.parse(fs.readFileSync('talker.json'));
+  if (!talker.length) {
+    res.status(HTTP_OK_STATUS).json([]);
+  }
+  res.status(HTTP_OK_STATUS).json(talker);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
